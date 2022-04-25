54f9c60a4e9cbb151927af33e770493ce61fa4ea
Jose Granados <jgranados.iastic@gmail.com>
2022-04-25 16:48:25 -0600

    ejemplos

--- IPC1_A_2022/src/main/java/com/jgranados/ipc1_a_2022/poo/avanzado/listas/ListaEnlazada.java
+++ IPC1_A_2022/src/main/java/com/jgranados/ipc1_a_2022/poo/avanzado/listas/ListaEnlazada.java
@@ -5,12 +5,11 @@ package com.jgranados.ipc1_a_2022.poo.avanzado.listas;
  * @author jose
  */
 public class ListaEnlazada<T> {
-    
+
     private Nodo<T> inicio;
     private Nodo<T> ultimo;
     private int longitud = 0;
-    
-    
+
     public void agregar(T contenido) {
         if (esVacia()) {
             inicio = new Nodo<>(contenido);
@@ -22,38 +21,62 @@ public class ListaEnlazada<T> {
         }
         longitud++;
     }
-    
+
     public boolean esVacia() {
         return inicio == null;
     }
-    
+
     public int obtenerLongitud() {
         return longitud;
     }
-    
-    public void eliminarElementoEnIndice(int indice) {
-        
-    }
-    
-    public void eliminarUltimo() {
-        
-    }
-    
-    public T obtenerContenido(int indice) {
-        
-    }
 
-    private Nodo<T> buscarIndice(int indice) throws Exception {
+    public void eliminarElementoEnIndice(int indice) throws ListaException {
         if (indice >= longitud || indice < 0) {
-            throw new Exception("Indice fuera de rango");
+            throw new ListaException("Indice fuera de rango");
         }
         
+        if (indice == 0) {
+            inicio = inicio.getSiguiente();
+        } else if (indice == longitud - 1) {
+            eliminarUltimo();
+        } else {
+            Nodo<T> anterior = buscarIndice(indice - 1);
+            Nodo<T> siguiente = anterior.getSiguiente().getSiguiente();
+            anterior.setSiguiente(siguiente);
+        }
+        longitud--;
+
+    }
+
+    public void eliminarUltimo() throws ListaException {
+        if (esVacia()) {
+            throw new ListaException("La lista esta vacia!");
+        } else if (longitud == 1) {
+            inicio = null;
+            ultimo = null;
+        } else {
+            Nodo<T> nuevoUltimo = buscarIndice(longitud - 2);
+            nuevoUltimo.setSiguiente(null);
+            ultimo = nuevoUltimo;
+        }
+        longitud--;
+    }
+
+    public T obtenerContenido(int indice) throws ListaException {
+        return buscarIndice(indice).getContenido();
+    }
+
+    public Nodo<T> buscarIndice(int indice) throws ListaException {
+        if (indice >= longitud || indice < 0) {
+            throw new ListaException("Indice fuera de rango");
+        }
+
         Nodo<T> actual = inicio;
         for (int i = 0; i < indice; i++) {
             Nodo<T> siguiente = actual.getSiguiente();
             actual = siguiente;
         }
-        
+
         return actual;
     }
 }
